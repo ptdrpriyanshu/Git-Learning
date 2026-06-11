315031f6f53e5768a74b68c4d57a1a1257932938
Priyanshu Patidar <your-en23cs301798@gmail.com>
2026-06-11 13:05:01 +0530

    UPI  done

--- first.js
+++ first.js
@@ -9,3 +9,6 @@ const footer="footer added in our website";
 console.log(footer);
 const Payment="integrated the payment gateway"
 console.log(Payment)
+
+const upi="integrted upi";
+console.log(upi);
